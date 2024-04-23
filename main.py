d72ebdb6e535aaa921b97ebb6e94747e92fe0e9d
RomaProger1 <101552538+RomaProger1@users.noreply.github.com>
2024-04-23 15:41:13 +0700

    V 0.3.2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
 import uvicorn
-from fastapi import FastAPI, Response, Path, Query, Body, Header
-from fastapi.responses import HTMLResponse, PlainTextResponse, JSONResponse, FileResponse
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from Public.users import users_router, classes_router
 from db import f
+import os
 
 # Загружаем переменные окружения из файла .env
 load_dotenv()
@@ -19,4 +20,5 @@ def p_index():
     return "<b> Hello World, FastAPI </b>"
 
 if __name__ == "__main__":
-   uvicorn.run(app,host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Используем порт, указанный в переменной окружения, или 8000 по умолчанию
+    uvicorn.run(app, host="0.0.0.0", port=port)  # Запускаем сервер на всех интерфейсах
