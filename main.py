02c469aa03b2e601830e0bcd22e7870fd9bb864f
RomaProger1 <101552538+RomaProger1@users.noreply.github.com>
2024-04-23 15:05:06 +0700

    V 0.3.1

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from dotenv import load_dotenv
 import uvicorn
 from fastapi import FastAPI, Response, Path, Query, Body, Header
 from fastapi.responses import HTMLResponse, PlainTextResponse, JSONResponse, FileResponse
 from Public.users import users_router, classes_router
 from db import f
+
+# Загружаем переменные окружения из файла .env
+load_dotenv()
 
 app = FastAPI()
 f()
@@ -10,8 +14,6 @@ f()
 app.include_router(users_router)
 app.include_router(classes_router())
 
-
-app.include_router(users_router)
 @app.get('/', response_class=HTMLResponse)
 def p_index():
     return "<b> Hello World, FastAPI </b>"
